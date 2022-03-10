81e90ad9c2dbf46a52626879ae155aef527bdc09
Maksym Rekunenko <61715859+Maks-xex@users.noreply.github.com>
2022-03-10 11:06:54 +0100

    Delete map.js

--- js/map.js
+++
@@ -1,9 +0,0 @@
-const data = "https://js.dump.academy/kekstagram/data";
-const dataPost = "https://js.dump.academy/kekstagram";
-let onload = fetch(data);
-let response = await fetch(dataPost);
-if (response.ok) {
-  let json = await response.json();
-} else {
-  alert("Статус Ошибки: " + response.status);
-}
